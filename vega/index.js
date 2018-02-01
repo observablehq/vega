9ef44c22c7bf84c010ad38f86c98e953bc17e8a6
Mike Bostock <mbostock@gmail.com>
2018-01-31 21:05:06 -0800

    Add Vega wrapper. Fixes #3.

---
+++ vega/index.js
@@ -0,0 +1,7 @@
+define(["vega@3/build/vega.min.js"], function(Vega) {
+  return function vega(spec) {
+    var div = document.createElement("div");
+    var view = new Vega.View(Vega.parse(spec));
+    return view.initialize(div).runAsync().then(function() { return div; });
+  };
+});
