1f0476a1698ff831d01efa09361b0e210af1ea83
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-04 20:03:40 -0600

    Allow an optional `renderer` to be passed.

--- vega-lite/index.js
+++ vega-lite/index.js
@@ -1,7 +1,12 @@
 define(["vega@3/build/vega.min.js", "vega-lite@2/build/vega-lite.min.js"], function(Vega, VegaLite) {
-  return function vegalite(spec) {
+  return function vegalite(spec, options) {
     var div = document.createElement("div");
-    var view = new Vega.View(Vega.parse(VegaLite.compile(spec).spec));
-    return view.initialize(div).renderer("svg").runAsync().then(function() { return div; });
+    var view = new Vega.View(Vega.parse(VegaLite.compile(spec).spec)).initialize(div);
+
+    if (options && options.renderer) {
+      view = view.renderer(options.renderer);
+    }
+
+    return view.runAsync().then(function() { return div; });
   };
 });
--- vega/index.js
+++ vega/index.js
@@ -1,7 +1,12 @@
 define(["vega@3/build/vega.min.js"], function(Vega) {
-  return function vega(spec) {
+  return function vega(spec, options) {
     var div = document.createElement("div");
-    var view = new Vega.View(Vega.parse(spec));
-    return view.initialize(div).renderer("svg").runAsync().then(function() { return div; });
+    var view = new Vega.View(Vega.parse(spec)).initialize(div);
+
+    if (options && options.renderer) {
+      view = view.renderer(options.renderer);
+    }
+
+    return view.runAsync().then(function() { return div; });
   };
 });
