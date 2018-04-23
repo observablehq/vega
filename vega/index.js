14a68c57cc72ac3001919f05b619872548fbf8cb
Dominik Moritz <domoritz@gmail.com>
2018-04-22 22:24:22 -0700

    Don’t use bundle, hide actions unless the user hovers over the Vega chart, attach style only once

--- vega/index.js
+++ vega/index.js
@@ -1,8 +1,12 @@
-define(["https://bundle.run/vega-embed@3"], function(VegaEmbed) {
-  var style = document.createElement("style")
-  style.innerHTML = ".vega-embed .vega-actions > a { visibility: hidden; font-size: 0.9em; margin-right: 0.6em; } " + 
-    ".vega-embed:hover .vega-actions > a { visibility: visible; }"
-  document.getElementsByTagName("head")[0].appendChild(style)
+define(["vega-embed@3"], function(VegaEmbed) {
+  var ID = 'vega-embed-style';
+  if (!document.getElementById(ID)) {
+    var style = document.createElement("style")
+    style.id = ID
+    style.innerHTML = ".vega-embed .vega-actions > a { opacity: 0.2; font-size: 0.9em; margin-right: 0.6em; } " + 
+      ".vega-embed:hover .vega-actions > a { opacity: 1; }"
+    document.getElementsByTagName("head")[0].appendChild(style)
+  }
 
   return function vegalite(spec, options) {
     var div = document.createElement("div");
