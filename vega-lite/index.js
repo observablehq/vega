b8c7ddf771d3a7c91309a2e431db1cf15d7314c1
Mike Bostock <mbostock@gmail.com>
2018-08-15 07:32:03 -0700

    Specify dependency versions in package.json.

--- vega-lite/index.js
+++ vega-lite/index.js
@@ -1,4 +1,4 @@
-define(["vega@3/build/vega.min.js", "vega-lite@2/build/vega-lite.min.js"], function(Vega, VegaLite) {
+define(["vega", "vega-lite"], function(Vega, VegaLite) {
   return function vegalite(spec) {
     var div = document.createElement("div");
     var view = new Vega.View(Vega.parse(VegaLite.compile(spec).spec));
