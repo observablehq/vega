3b1a1def73f1c104d641e967fe634aaba83ac00c
Mike Bostock <mbostock@gmail.com>
2018-08-19 08:09:46 -0700

    Expose view.

--- vega-lite/index.js
+++ vega-lite/index.js
@@ -1,7 +1,7 @@
 define(["vega", "vega-lite"], function(Vega, VegaLite) {
   return function vegalite(spec) {
     var div = document.createElement("div");
-    var view = new Vega.View(Vega.parse(VegaLite.compile(spec).spec));
+    var view = div.value = new Vega.View(Vega.parse(VegaLite.compile(spec).spec));
     return view.initialize(div).runAsync().then(function() { return div; });
   };
 });
