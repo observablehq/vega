486b7be875cd8b2d51eef9016df7e2d382f9aad0
Dominik Moritz <domoritz@gmail.com>
2019-06-07 10:56:44 -0700

    Update Vega

--- vega/index.js
+++ vega/index.js
@@ -1,4 +1,4 @@
-define(["vega@3/build/vega.min.js"], function(Vega) {
+define(["vega@5/build/vega.min.js"], function(Vega) {
   return function vega(spec) {
     var div = document.createElement("div");
     var view = new Vega.View(Vega.parse(spec));
